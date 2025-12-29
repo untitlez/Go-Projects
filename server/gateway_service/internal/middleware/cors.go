c0c6ec85dc43bc7d199af314d53caab4637e110c
untitlez <tleza_championship2@hotmail.com>
2025-12-29 10:49:53 +0700

    fix : CORS

--- server/gateway_service/internal/middleware/cors.go
+++ server/gateway_service/internal/middleware/cors.go
@@ -9,6 +9,7 @@ func AuthCORS(app *fiber.App, domain string) {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     domain,
 		AllowCredentials: true,
+		AllowHeaders:     "Origin, Content-Type, Authorization",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 	}))
 }
