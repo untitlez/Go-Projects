01c8c2888db62d5e470ea3c9f7e28458c1291864
untitlez <tleza_championship2@hotmail.com>
2026-01-24 00:20:05 +0700

    fix : change file

--- server/user_service/internal/handler/handler_create.go
+++ server/user_service/internal/handler/handler_create.go
@@ -5,19 +5,6 @@ import (
 	"github.com/untitlez/E-Commerce.git/internal/domain"
 )
 
-func (h *handler) SignUp(c *fiber.Ctx) error {
-	req := &domain.UserRequest{}
-	if errBodyParser := c.BodyParser(req); errBodyParser != nil {
-		return errBodyParser
-	}
-
-	if err := h.sv.SignUp(req); err != nil {
-		return h.responseError(c, 400, err)
-	}
-
-	return h.responseSuccess(c, 200, "", nil)
-}
-
 func (h *handler) SignIn(c *fiber.Ctx) error {
 	req := &domain.UserRequest{}
 	if errBodyParser := c.BodyParser(req); errBodyParser != nil {
@@ -30,4 +17,17 @@ func (h *handler) SignIn(c *fiber.Ctx) error {
 	}
 
 	return h.responseSuccess(c, 200, "", res)
+}
+
+func (h *handler) SignUp(c *fiber.Ctx) error {
+	req := &domain.UserRequest{}
+	if errBodyParser := c.BodyParser(req); errBodyParser != nil {
+		return errBodyParser
+	}
+
+	if err := h.sv.SignUp(req); err != nil {
+		return h.responseError(c, 400, err)
+	}
+
+	return h.responseSuccess(c, 200, "", nil)
 }
